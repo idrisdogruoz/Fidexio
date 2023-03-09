246ed7994067daa65c6550c1100759efdc5ae1b7
idris <idrisdogruoz@gmail.com>
2023-03-09 17:40:31 +0300

    utilities

---
+++ src/test/java/com/fidexio_project/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package com.fidexio_project.pages;
+
+import com.fidexio_project.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "login")
+    public WebElement email;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(css = ".btn.btn-primary")
+    public WebElement loginBtn;
+
+    @FindBy(css = ".alert.alert-danger")
+    public WebElement wrongLoginError;
+
+}
