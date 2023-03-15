2306fa56f33080d9872c6a8d203165a1ac26bce8
idrisDogruoz <111895940+idrisdogruoz@users.noreply.github.com>
2023-03-15 15:28:51 +0300

    utilities

--- src/test/java/com/fidexio/step_definitions/Log_out_step_definitions.java
+++ src/test/java/com/fidexio/step_definitions/Log_out_step_definitions.java
@@ -1,47 +1,61 @@
 package com.fidexio.step_definitions;
 
+import com.fidexio.pages.LoginPage;
 import com.fidexio.pages.LogoutPage;
+import com.fidexio.utils.ConfigurationReader;
+import com.fidexio.utils.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class Log_out_step_definitions {
 
-LogoutPage logoutPage = new LogoutPage();
+    LogoutPage logoutPage = new LogoutPage();
+    LoginPage loginPage = new LoginPage();
 
     @Given("user  is on the login page")
     public void user_is_on_the_login_page() {
-       logoutPage.usernamebtn.isDisplayed();
+        Driver.getDriver().get(ConfigurationReader.getProperty("environment"));
+
     }
 
-
-    @And("user logged in with  valid credential")
-    public void userLoggedInWithValidCredential() {
-
+    @Given("user logged in with  valid credential")
+    public void user_logged_in_with_valid_credential() {
+        loginPage.email.sendKeys("posmanager11@info.com");
+        loginPage.password.sendKeys("posmanager");
+        loginPage.loginBtn.click();
     }
 
     @When("user clicks username at the right top  of  the  corner")
-    public void userClicksUsernameAtTheRightTopOfTheCorner() {
+    public void user_clicks_username_at_the_right_top_of_the_corner() {
+        logoutPage.usernamebtn.isDisplayed();
     }
 
-    @And("user sees log out button")
-    public void userSeesLogOutButton() {
-    }
-
-    @And("user clicks log out button")
-    public void userClicksLogOutButton() {
+    @When("user clicks log out button")
+    public void user_clicks_log_out_button() {
+        logoutPage.usernamebtn.click();
+        logoutPage.logoutbtn.click();
     }
 
     @Then("user  goes back to login page")
-    public void userGoesBackToLoginPage() {
+    public void user_goes_back_to_login_page() {
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Login | Best solution for startups";
+        Assert.assertEquals(expectedTitle, actualTitle);
     }
 
     @And("user press back key on the keyboard")
     public void userPressBackKeyOnTheKeyboard() {
+        loginPage.loginBtn.sendKeys(Keys.BACK_SPACE);
     }
 
     @And("user is still on the page")
     public void userIsStillOnThePage() {
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Login | Best solution for startups";
+        Assert.assertEquals(expectedTitle, actualTitle);
     }
 }
--- src/test/resources/logout.feature
+++ src/test/resources/logout.feature
@@ -8,13 +8,11 @@ Feature: Log out functionality
 
   Scenario: User  can log out and ends up in  login page
     When user clicks username at the right top  of  the  corner
-    And user sees log out button
     And user clicks log out button
     Then user  goes back to login page
 
   Scenario: User  can not go to the  home  page  again by clicking the step back button after successfully logged out
     When  user clicks username at the right top  of  the  corner
-    And user sees log out button
     And user clicks log out button
     Then  user  goes back to login page
     And user press back key on the keyboard
